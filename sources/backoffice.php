01aa749afaaf426c45d18da683064b9e0f2e6785
Matéo Alves <backwolf4728@gmail.com>
2022-12-08 12:23:10 +0100

    Insertion du tableau XML

--- sources/backoffice.php
+++ sources/backoffice.php
@@ -1,6 +1,15 @@
 <?php
     try {
-        //code...
+
+        echo "<html>";
+        echo "<head>";
+        echo "<title>BACK OFFICE</title>";
+        echo "</head>";
+
+        echo "<body>";
+        
+        echo "</body>";
+
     } catch (Throwable $e) {
         echo 'Erreur : '.$e->getMessage().'<br />';
         die();
