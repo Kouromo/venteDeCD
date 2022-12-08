162fa6f50bfebd24e10bad56c99a68ffade1310d
Quentin Robert <robertquentin29@gmail.com>
2022-12-08 10:52:10 +0100

    ajout d'un dossier image ainsi que mise a jour du fichier accueil.php avec un header

---
+++ sources/accueil.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<HTML>
+    <head>
+        <meta charset="UTF-8" />
+        <link rel="stylesheet" href="accueil.css" />
+        <script src="https://kit.fontawesome.com/7c2235d2ba.js" crossorigin="anonymous"></script>
+        <title>CDSpeed</title>
+    </head>
+
+    <body>
+        <header>
+            
+        </header>
+    </body>
+
+    <footer>
+
+    </footer>
+</HTML>
