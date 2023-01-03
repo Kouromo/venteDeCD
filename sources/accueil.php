399394a1a2a54edfa6cfc3336001457837904836
Quentin Robert <robertquentin29@gmail.com>
2023-01-03 23:37:42 +0100

    modification du fichier XML afin qu'il y ait seulement le nom du fichier image à mettre, modification de l'accueil, et du detail .php pour gérer la modification du XML ainsi qu'optimiser le code et modification du CSS pour que ce soit JOLI

--- sources/accueil.php
+++ sources/accueil.php
@@ -28,28 +28,47 @@
                 // Charge le fichier XML dans un objet SimpleXML
                 $xml = simplexml_load_file('../BD/bd.xml');
 
+                // Initialise le compteur à 0
+                $counter = 0;
+
                 // Parcours chaque enregistrement de CD dans le fichier XML
                 foreach ($xml->cd as $cd)
                 {
-                    echo '<a href="details.php?id=' . $cd->id . '"><img class="image" src="../BD/' . $cd->image . '" alt="Texte alternatif"></a>';
-                }
-
-                foreach ($xml->cd as $cd)
-                {
+                    // Si le compteur atteint 6, arrête la boucle
+                    if ($counter == 6) {
+                        break;
+                    }
+                    echo '<a href="details.php?id=' . $cd->id . '"><img class="image" src="../BD/Images/' . $cd->image . '" alt="Texte alternatif"></a>';
                     // Affiche le titre du CD entre des balises h2
-                    echo '<h2>' . $cd->titre . '</h2>';
-                }
-
-                foreach ($xml->cd as $cd)
-                {
+                    echo '<h2 class="titre">' . $cd->titre . '</h2>';
                     // Affiche l'artiste du CD entre des balises p
-                    echo '<p>' . $cd->auteur . '</p>';
+                    echo '<p class="auteur">' . $cd->auteur . '</p>';
+                    // Affiche le prix du CD entre des balises p
+                    echo '<p class="prix">' . $cd->prix . '</p>';
+                    // Incrémente le compteur
+                    $counter++;
                 }
 
-                foreach ($xml->cd as $cd)
-                {
+                $counter = 0;
+                foreach ($xml->cd as $cd) {
+                    if ($counter < 6) {
+                        $counter++;
+                        continue;
+                    }
+                    // code à exécuter sur chaque élément à partir de la 7ème itération
+                    // Si le compteur atteint 6, arrête la boucle
+                    if ($counter == 12) {
+                        break;
+                    }
+                    echo '<a href="details.php?id=' . $cd->id . '"><img class="image2" src="../BD/Images/' . $cd->image . '" alt="Texte alternatif"></a>';
+                    // Affiche le titre du CD entre des balises h2
+                    echo '<h2 class="titre2">' . $cd->titre . '</h2>';
+                    // Affiche l'artiste du CD entre des balises p
+                    echo '<p class="auteur2">' . $cd->auteur . '</p>';
                     // Affiche le prix du CD entre des balises p
-                    echo '<p>' . $cd->prix . '</p>';
+                    echo '<p class="prix2">' . $cd->prix . '</p>';
+                    // Incrémente le compteur
+                    $counter++;
                 }
             ?>
         </main>
