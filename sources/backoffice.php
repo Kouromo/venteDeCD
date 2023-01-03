f819cf68a41da9f8866621c82ba59ece86ad5caf
Matéo Alves <blackwolf4728@gmail.com>
2023-01-04 00:26:47 +0100

    Erreur Suppression

--- sources/backoffice.php
+++ sources/backoffice.php
@@ -7,27 +7,35 @@
         echo "</head>";
         
         echo "<body>";
-        echo "<form method=post action=ajouterCD.php>";
-        echo "<label for=title>Titre :</label><br>";
-        echo "<input type=text name=title id=title><br>";
-        echo "<label for=artist>Artiste :</label><br>";
-        echo "<input type=text name=artist id=artist><br>";
-        echo "<label for=genre>Genre :</label><br>";
-        echo "<input type=text name=genre id=genre><br>";
-        echo "<label for=price>Prix :</label><br>";
-        echo "<input type=text name=price id=price><br>";
-        echo "<label for=album>Album :</label><br>";
-        echo "<input type=text name=album id=album><br>";
-        echo "<label for=image>Image :</label><br>";
-        echo "<input type=file name=image id=image><br><br>";
-        echo "<input type=submit value=Envoyer>";
+        echo "<h1>Back-Office</h1>";
+        echo "<hr>";
+        echo "<form method='post' action='ajouterCD.php'>";
+        echo "<h2>Ajouter un titre à la BD</h2>";
+        echo "<label for='title'>Titre :</label><br>";
+        echo "<input type='text' name='title' id='title'><br>";
+        echo "<label for='artist'>Artiste :</label><br>";
+        echo "<input type='text' name='artist' id='artist'><br>";
+        echo "<label for='genre'>Genre :</label><br>";
+        echo "<input type='text' name='genre' id='genre'><br>";
+        echo "<label for='price'>Prix :</label><br>";
+        echo "<input type='text' name='price' id='price'><br>";
+        echo "<label for='album'>Album :</label><br>";
+        echo "<input type='text' name='album' id='album'><br>";
+        echo "<label for='image'>Image :</label><br>";
+        echo "<input type='file' name='image' id='image'><br><br>";
+        echo "<input type='submit' value='Envoyer'>";
         echo "</form>";
+        echo "<hr>";
 
-        echo "<form method=post action=supprimerCD.php>";
-        echo "<label for=title>Titre :</label><br>";
-        echo "<input type=text name=title id=titleDelete><br>";
-        echo "<input type=submit value=Envoyer>";
+        // Formulaire de suppression de CD
+        echo "<form method='post' action='supprimerCD.php'>";
+        echo "<h2>Supprimer un titre de la BD</h2>";
+        echo "<label for='titleDelete'>Titre à supprimer :</label><br>";
+        echo "<input type='text' name='titleDelete' id='titleDelete'><br>";
+        echo "<input type='submit' value='Envoyer'>";
         echo "</form>";
+        
+        // Fin du corps HTML
         echo "</body>";
         echo "</html>";
 
--- sources/supprimerCD.php
+++ sources/supprimerCD.php
@@ -1,5 +1,15 @@
 <?php
     $titleDelete = $_POST['titleDelete'];
-
     $filename = '../BD/bd.xml';
+
+    $xml = simplexml_load_file($filename);
+
+    foreach ($xml->cd as $cd) {
+        if ($cd->titre == $titleDelete) {
+            unset($cd[0]);
+        }
+    }
+    
+
+    file_put_contents($filename, $xml->asXML());
 ?>
